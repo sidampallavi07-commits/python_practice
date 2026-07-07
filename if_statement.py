180f930ecd221a9afe8ffd08c5cd338c2e895b03
Pallavi <sidampallavi07@gmail.com>
2026-07-07 16:13:37 +0530

    if_statement file added

---
+++ if_statement.py
@@ -0,0 +1,3 @@
+a=int(input("enter the value"))
+if a%2==0:
+    print(a,"enter the even number")
---
+++ if_statement_.py
@@ -0,0 +1,6 @@
+#if statement
+age = int (input("enter your age"))
+if age >16:
+Print("you can apply for licence")
+Print ("speed thrills but kills")
+
